94da5d68d2200c4896ef7294b07efd99325dc539
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2022-03-06 01:20:49 +0800

    Update: Test passed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,16 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
   
+  def new
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
 end
